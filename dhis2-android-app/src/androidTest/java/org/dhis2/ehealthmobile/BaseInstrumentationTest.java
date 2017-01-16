cc24bc9c68215d9ebf77daddc7ab59925f2dc149
lelloman <cerasuolodomenico@gmail.com>
2017-01-16 18:24:24 +0100

    test: add login activity test

--- dhis2-android-app/src/androidTest/java/org/dhis2/ehealthmobile/BaseInstrumentationTest.java
+++ dhis2-android-app/src/androidTest/java/org/dhis2/ehealthmobile/BaseInstrumentationTest.java
@@ -1,9 +1,11 @@
 package org.dhis2.ehealthmobile;
 
 import android.os.RemoteException;
+import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
 
 import org.junit.Before;
+import org.junit.runner.RunWith;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
@@ -15,7 +17,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.IsNot.not;
 
-public class BaseInstrumentationTest {
+@RunWith(AndroidJUnit4.class)
+public abstract class BaseInstrumentationTest {
 
 	protected UiDevice device;
 
--- dhis2-android-app/src/androidTest/java/org/dhis2/ehealthmobile/LoginActivityTest.java
+++ dhis2-android-app/src/androidTest/java/org/dhis2/ehealthmobile/LoginActivityTest.java
@@ -1,7 +1,9 @@
 package org.dhis2.ehealthmobile;
 
+import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 
+import com.squareup.okhttp.mockwebserver.MockResponse;
 import com.squareup.okhttp.mockwebserver.MockWebServer;
 
 import org.dhis2.ehealthMobile.R;
@@ -10,7 +12,19 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.net.HttpURLConnection;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsNot.not;
+
 public class LoginActivityTest extends BaseInstrumentationTest{
+
+	private static final String USERNAME = "username";
+	private static final String PASSWORD = "password";
 
 	MockWebServer server;
 
@@ -23,14 +37,70 @@ public class LoginActivityTest extends BaseInstrumentationTest{
 		server = new MockWebServer();
 	}
 
+	protected void typeLoginData(String url, String username, String password){
+
+		typeTextInView(R.id.server_url, url);
+		Espresso.closeSoftKeyboard();
+		typeTextInView(R.id.username, username);
+		Espresso.closeSoftKeyboard();
+		typeTextInView(R.id.password, password);
+		Espresso.closeSoftKeyboard();
+	}
+
+	protected void failLogin(String url, String username, String password, int responseCode, int toastMessage){
+
+		server.enqueue(new MockResponse()
+				.setResponseCode(responseCode)
+				.setBody("")
+				.addHeader("Content-Type", "application/json"));
+
+		typeLoginData(url, username, password);
+
+		clickView(R.id.login_button);
+		onView(withText(toastMessage)).inRoot(withDecorView(not(is(rule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+	}
+
 
 	@Test
-	public void loginShouldFail(){
+	public void loginScreenShouldShowSomeViews(){
+
 		checkViewDisplayed(R.id.dhis2_logo);
 		checkViewDisplayed(R.id.password);
 		checkViewDisplayed(R.id.username);
 		checkViewDisplayed(R.id.server_url);
 		checkViewDisplayed(R.id.login_button);
+
 		checkViewNotDisplayed(R.id.progress_bar);
+	}
+
+	@Test
+	public void loginScreenShouldRetainInputDataOnRotation(){
+
+		rotateNatural();
+
+		typeLoginData(server.url("").toString(), USERNAME, PASSWORD);
+
+		rotateLeft();
+		rotateRigt();
+		rotateNatural();
+
+		checkViewWithTextIsDisplayed(server.url("").toString());
+		checkViewWithTextIsDisplayed(USERNAME);
+		checkViewWithTextIsDisplayed(PASSWORD);
+	}
+
+	@Test
+	public void loginShouldFailBecauseOfWrongCredentials() {
+		failLogin(server.url("foo/bar").toString(), USERNAME, PASSWORD,HttpURLConnection.HTTP_UNAUTHORIZED, R.string.wrong_username_password);
+	}
+
+	@Test
+	public void loginShouldFailBecauseOfWrongUrl() {
+		failLogin("i'm not an url", USERNAME, PASSWORD, HttpURLConnection.HTTP_NOT_FOUND, R.string.wrong_url);
+	}
+
+	@Test
+	public void loginShouldFailBecauseOfUnexpectedReason(){
+		failLogin(server.url("").toString(), USERNAME, PASSWORD, Integer.MIN_VALUE, R.string.try_again);
 	}
 }
