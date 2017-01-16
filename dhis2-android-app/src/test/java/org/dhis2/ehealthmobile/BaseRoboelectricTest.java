a78dbd152a68367dbe178e6c144efffc00e02b72
lelloman <cerasuolodomenico@gmail.com>
2017-01-16 15:50:23 +0100

    refactor: roboelectric test can load json models from text file in resources

--- dhis2-android-app/src/test/java/org/dhis2/ehealthmobile/BaseRoboelectricTest.java
+++ dhis2-android-app/src/test/java/org/dhis2/ehealthmobile/BaseRoboelectricTest.java
@@ -6,18 +6,56 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
 import org.dhis2.ehealthMobile.BuildConfig;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 22)
 public abstract class BaseRoboelectricTest {
 
+	private static final Gson gson = new Gson();
+
 	protected Context getContext(){
 		return RuntimeEnvironment.application;
+	}
+
+	protected <T> T loadJson(String filename, Class<T> clazz){
+		return gson.fromJson(loadJsonString(filename), clazz);
+	}
+
+	protected String loadJsonString(String filename){
+
+		if(!filename.endsWith(".json"))
+			filename = filename + ".json";
+
+		return readRawTextFile(String.format("json/%s", filename));
+	}
+
+	protected String readRawTextFile(String filename){
+
+		try {
+			InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filename);//getContext().getResources().openRawResource(id);
+			BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
+			StringBuilder builder = new StringBuilder();
+			String line;
+			while ((line = r.readLine()) != null) {
+				builder.append(line).append('\n');
+			}
+
+			return builder.toString();
+		}catch (Exception e){
+			e.printStackTrace();
+			return "";
+		}
 	}
 
 	protected TextView textViewWithText(View view, String text) {
--- dhis2-android-app/src/test/java/org/dhis2/ehealthmobile/HeaderRowTest.java
+++ dhis2-android-app/src/test/java/org/dhis2/ehealthmobile/HeaderRowTest.java
@@ -5,6 +5,7 @@ import android.view.View;
 
 import com.google.gson.Gson;
 
+import org.dhis2.ehealthMobile.R;
 import org.dhis2.ehealthMobile.io.models.configfile.FieldGroup;
 import org.dhis2.ehealthMobile.ui.adapters.dataEntry.rows.FieldType;
 import org.dhis2.ehealthMobile.ui.adapters.dataEntry.rows.HeaderRow;
@@ -14,49 +15,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class HeaderRowTest extends BaseRoboelectricTest{
 
-	Gson gson = new Gson();
-
-	String columnWithLabelsJson = "{\n" +
-			"\t\"columns\" : [\n" +
-			"\t\t{\t\n" +
-			"\t\t\t\"label\": \"label0\",\n" +
-			"\t\t\t\"columns\": [\n" +
-			"\t\t\t\t{\"label\": \"label00\"},\n" +
-			"\t\t\t\t{\"label\": \"label01\"}\n" +
-			"\t\t\t]\n" +
-			"\t\t},\n" +
-			"\t\t{\n" +
-			"\t\t\t\"label\": \"label1\",\n" +
-			"\t\t\t\"columns\": [\n" +
-			"\t\t\t\t{\"label\": \"label10\"},\n" +
-			"\t\t\t\t{\"label\": \"label11\"}\n" +
-			"\t\t\t]\n" +
-			"\t\t}\n" +
-			"\t]\t\n" +
-			"}";
-/*
-{
-	"columns" : [
-		{
-			"label": "label0",
-			"columns": [
-				{"label": "label00"},
-				{"label": "label01"}
-			]
-		},
-		{
-			"label": "label1",
-			"columns": [
-				{"label": "label10"},
-				{"label": "label11"}
-			]
-		}
-	]
-}
- */
 	@Test
 	public void headerRowViewShouldHaveAllColumnLabels(){
-		FieldGroup fieldGroup = gson.fromJson(columnWithLabelsJson, FieldGroup.class);
+		FieldGroup fieldGroup = loadJson("columns_with_labels", FieldGroup.class);
 		HeaderRow headerRow = new HeaderRow(fieldGroup, LayoutInflater.from(getContext()));
 
 		View headerRowView = headerRow.getView(0, null);
@@ -78,34 +39,14 @@ public class HeaderRowTest extends BaseRoboelectricTest{
 		assertThat(headerRow.getViewType()).isEqualTo(FieldType.values().length * 10);
 
 
-
-		String jsonString =
-"{\n" +
-	"\"columns\": [" +
-		"{" +
-			"\"columns\": [{},{}]" +
-		"}," +
-		"{" +
-			"\"columns\": [{},{}]" +
-		"}" +
-	"]" +
-"}";
-		fieldGroup = gson.fromJson(jsonString, FieldGroup.class);
+		fieldGroup = loadJson("columns_2_2", FieldGroup.class);
 		headerRow = new HeaderRow(fieldGroup, null);
 		assertThat(headerRow.getViewType()).isEqualTo(
 				Integer.valueOf(String.format("%s%s%s%s", FieldType.values().length, 2, 2, 0))
 		);
 
 
-		jsonString =
-"{\n" +
-	"\"columns\": [" +
-		"{" +
-			"\"columns\": [{},{}, {}]" +
-		"}" +
-	"]" +
-"}";
-		fieldGroup = gson.fromJson(jsonString, FieldGroup.class);
+		fieldGroup = loadJson("columns_1_3", FieldGroup.class);
 		headerRow = new HeaderRow(fieldGroup, null);
 		assertThat(headerRow.getViewType()).isEqualTo(
 				Integer.valueOf(String.format("%s%s%s%s", FieldType.values().length, 1, 3, 0))
@@ -113,21 +54,7 @@ public class HeaderRowTest extends BaseRoboelectricTest{
 
 
 		// broken layout, for now throws no exception
-		jsonString =
-"{\n" +
-	"\"columns\": [" +
-		"{" +
-			"\"columns\": [{},{}, {}]" +
-		"}," +
-		"{" +
-			"\"columns\": [{}]" +
-		"}," +
-		"{" +
-			"\"columns\": [{}]" +
-		"}" +
-	"]" +
-"}";
-		fieldGroup = gson.fromJson(jsonString, FieldGroup.class);
+		fieldGroup = loadJson("columns_broken", FieldGroup.class);
 		headerRow = new HeaderRow(fieldGroup, null);
 		assertThat(headerRow.getViewType()).isEqualTo(
 				Integer.valueOf(String.format("%s%s%s%s", FieldType.values().length, 3, 3, 0))
